b53c0afeb1f63a75e76b330bb9d32817c082b0ab
Christoph Grundmann <christoph.grundmann@gmail.com>
2016-03-18 22:53:18 +0100

    added transition state machine

---
+++ AsyncStateMachine/AsyncStateMachine/Transition.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncStateMachine
+{
+    public class Transition
+    {
+        private IState CurrentState { get; set; }
+
+        /// <summary>
+        /// Checks whether this Transistion is currently pending
+        /// </summary>
+        /// <returns>Returns true if this transition is pending, otherwise false</returns>
+        public bool IsPending => CurrentState.GetType() == typeof (PendingState);
+
+        /// <summary>
+        /// Checks whether this Transistion is currently running
+        /// </summary>
+        /// <returns>Returns true if this transition is running, otherwise false</returns>
+        public bool IsRunning => CurrentState.GetType() == typeof (RunningState);
+
+        /// <summary>
+        /// Checks whether this Transistion is interrupted
+        /// </summary>
+        /// <returns>Returns true if this transition is interrupted, otherwise false</returns>
+        public bool IsInterrupted => CurrentState.GetType() == typeof(InterruptedState);
+
+        /// <summary>
+        /// Checks whether this Transistion is completed
+        /// </summary>
+        /// <returns>Returns true if this transition is completed, otherwise false</returns>
+        public bool IsCompleted => CurrentState.GetType() == typeof(CompletedState);
+
+        public Transition()
+        {
+            CurrentState = new PendingState();
+        }
+
+        public void Execute()
+        {
+            CurrentState.Execute(this);
+        }
+
+        public void Interrupt()
+        {
+            CurrentState.Interrupt(this);
+        }
+
+        public void Complete()
+        {
+            CurrentState.Complete(this);
+        }
+
+        #region States
+
+        public interface IState
+        {
+            void Execute(Transition transition);
+            void Interrupt(Transition transition);
+            void Complete(Transition transition);
+        }
+
+        public class PendingState : IState
+        {
+            public void Execute(Transition transition)
+            {
+                transition.CurrentState = new RunningState();
+            }
+
+            public void Interrupt(Transition transition)
+            {
+                transition.CurrentState = new InterruptedState();
+            }
+
+            public void Complete(Transition transition)
+            {
+                // Unable to complete a pending state
+            }
+        }
+
+        public class RunningState : IState
+        {
+            public void Execute(Transition transition)
+            {
+                // Unable to execute an already running transition
+            }
+
+            public void Interrupt(Transition transition)
+            {
+                transition.CurrentState = new InterruptedState();
+            }
+
+            public void Complete(Transition transition)
+            {
+                transition.CurrentState = new CompletedState();
+            }
+        }
+
+        public class InterruptedState : IState
+        {
+            public void Execute(Transition transition)
+            {
+                // Unable to execute an interrupted transition
+            }
+
+            public void Interrupt(Transition transition)
+            {
+                // Unable to interrupt an already interrupted transition
+            }
+
+            public void Complete(Transition transition)
+            {
+                // Unable to complete an interrupted transition
+            }
+        }
+
+        public class CompletedState : IState
+        {
+            public void Execute(Transition transition)
+            {
+                // Unable to execute a completed transition
+            }
+
+            public void Interrupt(Transition transition)
+            {
+                // Unable to interrupt a completed transition
+            }
+
+            public void Complete(Transition transition)
+            {
+                // Unable to complete an already completed transition
+            }
+        }
+
+        #endregion
+    }   
+}
---
+++ AsyncStateMachine/AsyncStateMachineTests/TransitionTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AsyncStateMachine;
+
+namespace AsyncStateMachineTests
+{
+    [TestClass]
+    public class TransitionTests
+    {
+        private Transition TransitionUnderTest { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TransitionUnderTest = new Transition();
+        }
+
+        [TestMethod]
+        public void TestInitializingTransition()
+        {       
+            Assert.IsTrue(TransitionUnderTest.IsPending);            
+        }
+
+        [TestMethod]
+        public void TestExecutingTransition()
+        {
+            Assert.IsFalse(TransitionUnderTest.IsRunning);
+
+            TransitionUnderTest.Execute();
+            Assert.IsTrue(TransitionUnderTest.IsRunning);
+        }
+
+        [TestMethod]
+        public void TestInterruptingTransition()
+        {
+            Assert.IsFalse(TransitionUnderTest.IsInterrupted);
+
+            TransitionUnderTest.Interrupt();
+            Assert.IsTrue(TransitionUnderTest.IsInterrupted);
+        }
+
+        [TestMethod]
+        public void TestCompletingTransition()
+        {
+            TransitionUnderTest.Execute();
+            Assert.IsFalse(TransitionUnderTest.IsCompleted);
+
+            TransitionUnderTest.Complete();
+            Assert.IsTrue(TransitionUnderTest.IsCompleted);
+        }
+
+        [TestMethod]
+        public void TestThatTransitionCannotBeCompletedWhenPending()
+        {
+            Assert.IsTrue(TransitionUnderTest.IsPending);
+
+            TransitionUnderTest.Complete();
+            Assert.IsFalse(TransitionUnderTest.IsCompleted);
+            Assert.IsTrue(TransitionUnderTest.IsPending);
+        }
+
+        [TestMethod]
+        public void TestThatTransitionCannotBeCompletedIfInterrupted()
+        {
+            TransitionUnderTest.Interrupt();
+            Assert.IsTrue(TransitionUnderTest.IsInterrupted);
+
+            TransitionUnderTest.Complete();
+            Assert.IsFalse(TransitionUnderTest.IsCompleted);
+            Assert.IsTrue(TransitionUnderTest.IsInterrupted);
+        }
+    }
+
+  
+}
